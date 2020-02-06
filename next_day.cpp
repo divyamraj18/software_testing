7e084df933987ca696a14c1f0f19e4e64a245fe5
divyamraj18 <divyamraj18@gmail.com>
2020-02-06 05:47:13 +0530

    second program

--- next_day.cpp
+++ next_day.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
 using namespace std;
+int maxdays(int month,int year)
+{
+	int max=-1;
+	if(month==1||month==3||month==5||month==7||month==8||month==10||month==12)
+	{
+		max=31;
+	}
+	else if(month==4||month==6||month==9||month==11)
+	{
+		max=30;
+	}
+	else{
+		if((year%4==0&&year%100!=0)||year%400==0)
+			max=29;
+		else
+			max=28;
+	}
+	return max;
+}
+
 int main()
 {
 	int day,month,year;
@@ -10,20 +30,21 @@ int main()
 		cout<<"enter valid date in dd mm yyyy format";
 		cin>>day>>month>>year;
 	}
-	if(month==1||month==3||month==5||month==7||month==8||month==10||month==12)
+	day=day+1;
+	if(day>maxdays(month,year))
 	{
-		maxdays=31;
+		day=1;
+		month=month+1;
 	}
-	else if(month==4||month==6||month==9||month==11)
+	if(month>12)
 	{
-		maxdays=30;
+		month=1;
+		year=year+1;
 	}
-	else{
-		if((year%4==0&&year%100!=0)&&year%400==0)
-			maxdays=29;
-		else
-			maxdays=28;
-	}
+	cout<<day<<"-"<<month<<"-"<<year<<endl;
 
+	
+	
+	
 return 0;
 }
